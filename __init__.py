74fcdd2160877f94b69b9742220ad4189a697236
Bernat Brunet Torruella <bernat@nan-tic.com>
2014-05-21 17:11:20 +0200

    Make that the module could be installed and works :-)

--- __init__.py
+++ __init__.py
@@ -1,6 +1,10 @@
 #This file is part party_asterisk module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
-def register():
-    pass
+from trytond.pool import Pool
+from .contact_mechanism import *
 
+def register():
+    Pool.register(
+        ContactMechanism,
+        module='party_asterisk', type_='model')
