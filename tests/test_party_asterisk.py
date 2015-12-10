03d7f8e75541866e192c934a00fbf88f995a2c04
Sergi Almacellas Abellana <sergi@koolpi.com>
2015-12-10 23:52:59 +0100

    Migrate to version 3.8 (grafted from df4439190b3c00a85682f5c30b39ddf84203a888)

--- tests/test_party_asterisk.py
+++ tests/test_party_asterisk.py
@@ -1,31 +1,15 @@
-#!/usr/bin/env python
-#This file is part party_asterisk module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class PartyAsteriskTestCase(unittest.TestCase):
+class PartyAsteriskTestCase(ModuleTestCase):
     '''
     Test Party Asterisk module.
     '''
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('party_asterisk')
-
-    def test0005views(self):
-        '''
-        Test views.
-        '''
-        test_view('party_asterisk')
-
-    def test0006depends(self):
-        '''
-        Test depends.
-        '''
-        test_depends()
+    module = 'party_asterisk'
 
 
 def suite():
