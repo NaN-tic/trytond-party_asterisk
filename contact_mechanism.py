8a5bd279018dd85b7e0d08f5e2436880c4cd3473
?ngel ?lvarez <angel@nan-tic.com>
2018-02-01 11:06:11 +0100

    update to 4.7

--- contact_mechanism.py
+++ contact_mechanism.py
@@ -1,16 +1,16 @@
-#This file is part of party_asterisk module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+# This file is part of party_asterisk module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 from trytond.model import ModelView
 from trytond.pool import Pool, PoolMeta
 from trytond.pyson import Eval
 
 __all__ = ['ContactMechanism']
-__metaclass__ = PoolMeta
 
 
 class ContactMechanism():
     __name__ = "party.contact_mechanism"
+    __metaclass__ = PoolMeta
 
     @classmethod
     def __setup__(cls):
